b0bab2d59d3a23a4794055a9091745207f386d16
purnnendu <purnnendu.1695@gmail.com>
2021-08-03 22:30:17 +0530

    13 Roman to Integer in py

---
+++ 13 Roman to Integer/13 Roman to Integer.py
@@ -0,0 +1,38 @@
+"""
+Given a roman numeral, convert it to an integer.
+Example 1:
+
+Input: s = "III"
+Output: 3
+
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+"""
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
