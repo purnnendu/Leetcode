5306cb6d4194032b322f64ccd80d849d695a973f
purnnendu <purnnendu.1695@gmail.com>
2021-08-07 18:44:14 +0530

    21 Merge Two Sorted Lists using py

---
+++ 21 Merge Two Sorted Lists/21 Merge Two Sorted Lists.py
@@ -0,0 +1,56 @@
+"""
+Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Example 1:
+
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+
+Input: l1 = [], l2 = []
+Output: []
+
+Example 3:
+
+Input: l1 = [], l2 = [0]
+Output: [0]
+
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        ""
+        
+        # Alternate solution a bit slower
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1 or l2
+
+        """
+        dummy = ListNode(0)
+        temp = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                temp.next = l1
+                temp = temp.next
+                l1 = l1.next
+            else:
+                temp.next = l2
+                temp = temp.next
+                l2 = l2.next
+        if l1:
+            temp.next = l1
+        if l2:
+            temp.next = l2
+        return dummy.next
