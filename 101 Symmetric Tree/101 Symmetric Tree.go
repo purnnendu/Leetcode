c333290a96c16fd75ec1f87f7e3ed4890cc39045
purnnendu <purnnendu.1695@gmail.com>
2021-08-10 16:11:18 +0530

    101 Symmetric Tree using go

---
+++ 101 Symmetric Tree/101 Symmetric Tree.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ 
+func isMirror(node1 *TreeNode, node2 *TreeNode) bool {
+	if node1 == nil && node2 == nil {
+		return true
+	}
+
+	if node1 == nil || node2 == nil {
+		return false
+	}
+
+	return node1.Val == node2.Val && isMirror(node1.Right, node2.Left) && isMirror(node1.Left, node2.Right)
+}
+
+func isSymmetric(root *TreeNode) bool {
+    return isMirror(root, root)
+}
