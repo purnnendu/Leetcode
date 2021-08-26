4c3f0e52a129c20087615a7ce306aecd78e292a2
purnnendu <purnnendu.1695@gmail.com>
2021-08-26 12:29:59 +0530

    2 Add Two Numbers using py

---
+++ 2 Add Two Numbers/2 Add Two Numbers.py
@@ -0,0 +1,52 @@
+"""
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy_node = ListNode(0)
+        cur = dummy_node
+        carry = 0
+
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            cur_sum = v1+v2+carry
+            if cur_sum >= 10:
+                cur_sum -= 10
+                carry = 1
+                cur.next = ListNode(cur_sum)
+            else:
+                cur.next = ListNode(cur_sum)
+                carry = 0
+            cur = cur.next
+
+        if carry:
+            cur.next = carry
+        return dummy_node.next
