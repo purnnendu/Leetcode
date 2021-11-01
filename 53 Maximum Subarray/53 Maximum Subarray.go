c5b29236013f7b6ad669409c507ee7745c874bde
purnnendu <purnnendu.1695@gmail.com>
2021-11-01 14:02:34 +0530

    53 Maximum Subarray using go

---
+++ 53 Maximum Subarray/53 Maximum Subarray.go
@@ -0,0 +1,14 @@
+func maxSubArray(nums []int) int {
+    max, sum := nums[0],nums[0]
+	for _, v := range nums[1:] {
+		if sum < 0 {
+			sum = v
+		} else {
+			sum += v
+		}
+		if max < sum {
+			max = sum
+		}
+	}
+    return max
+}
