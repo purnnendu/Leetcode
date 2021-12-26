65927b160866ce4252757158e89b9588ad955c18
purnnendu <purnnendu.1695@gmail.com>
2021-12-26 16:11:32 +0530

    35 Search Insert Position using go

---
+++ 35 Search Insert Position/35 Search Insert Position.go
@@ -0,0 +1,16 @@
+func searchInsert(nums []int, target int) int {
+    low := 0
+    high := len(nums) - 1
+    for low <= high {
+        mid := (low + high)/2
+        if nums[mid] == target {
+            return mid
+        }
+        if nums[mid] < target {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return low
+}
