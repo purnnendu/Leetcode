0da7349093b61afd2246ed888f0c57169caf6fb3
purnnendu <purnnendu.1695@gmail.com>
2021-12-30 13:28:50 +0530

    876 Middle of the Linked List using go

---
+++ 876 Middle of the Linked List/876 Middle of the Linked List.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+    slow, fast := head, head
+    for fast.Next != nil && fast.Next.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+
+    if fast.Next != nil && fast.Next.Next == nil {
+        return slow.Next
+    }
+
+    return slow
+}
