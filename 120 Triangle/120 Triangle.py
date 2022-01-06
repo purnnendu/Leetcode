3b98a2a6f078ad9a762a6abef1a9f70105734e52
purnnendu <purnnendu.1695@gmail.com>
2022-01-06 18:20:06 +0530

    120 Triangle using py

---
+++ 120 Triangle/120 Triangle.py
@@ -0,0 +1,39 @@
+"""
+Given a triangle array, return the minimum path sum from top to bottom.
+For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+Example 1:
+
+Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+Output: 11
+Explanation: The triangle looks like:
+   2
+  3 4
+ 6 5 7
+4 1 8 3
+The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
+
+Example 2:
+
+Input: triangle = [[-10]]
+Output: -10
+"""
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        height = len(triangle)
+        dp = triangle
+        dp[0][0] = triangle[0][0]
+        #initialize two laterals
+        for i in range(1, height, 1):
+            dp[i][0] = triangle[i][0] + dp[i-1][0]
+            dp[i][i] = triangle[i][i] + dp[i-1][i-1]
+
+        for i in range(2, height, 1):
+            for j in range(1, i, 1):
+                dp[i][j] = min(dp[i-1][j],dp[i-1][j-1]) + triangle[i][j]
+
+        min_sum = float('inf')
+        for i in range(height):
+            if dp[height-1][i] <= min_sum:
+                min_sum = dp[height-1][i]
+        return min_sum
