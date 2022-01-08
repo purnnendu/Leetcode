179a5047eef446ddae29b2658e637f102764a71a
purnnendu <purnnendu.1695@gmail.com>
2022-01-08 13:13:57 +0530

    27 Remove Element using go

---
+++ 27 Remove Element/27 Remove Element.go
@@ -0,0 +1,14 @@
+func removeElement(nums []int, val int) int {
+    nlen := len(nums)
+	reslen := nlen
+	curIdx := 0
+	for i := 0; i < nlen; i++ {
+		if nums[i] == val {
+			reslen--
+			continue
+		}
+		nums[curIdx] = nums[i]
+		curIdx++
+	}
+    return reslen
+}
