b4c4ea245d804ab04c0a833ae5d1bf06e6b67eb7
purnnendu <purnnendu.1695@gmail.com>
2022-01-18 18:14:38 +0530

    475 Heaters using py

---
+++ 475 Heaters/475 Heaters.py
@@ -0,0 +1,36 @@
+"""
+Winter is coming! During the contest, your first job is to design a standard heater with a fixed warm radius to warm all the houses.
+Every house can be warmed, as long as the house is within the heater's warm radius range.
+Given the positions of houses and heaters on a horizontal line, return the minimum radius standard of heaters so that those heaters could cover all houses.
+Notice that all the heaters follow your radius standard, and the warm radius will the same.
+
+Example 1:
+
+Input: houses = [1,2,3], heaters = [2]
+Output: 1
+Explanation: The only heater was placed in the position 2, and if we use the radius 1 standard, then all the houses can be warmed.
+
+Example 2:
+
+Input: houses = [1,2,3,4], heaters = [1,4]
+Output: 1
+Explanation: The two heater was placed in the position 1 and 4. We need to use radius 1 standard, then all the houses can be warmed.
+
+Example 3:
+
+Input: houses = [1,5], heaters = [2]
+Output: 3
+"""
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses = sorted(houses)
+        heaters = sorted(heaters)
+
+        heaters=[float('-inf')]+heaters+[float('inf')] # add 2 fake heaters
+        ans,i = 0,0
+        for house in houses:
+            while house > heaters[i+1]:  # search to put house between heaters
+                i +=1
+            dis = min (house - heaters[i], heaters[i+1]- house)
+            ans = max(ans, dis)
+        return ans
