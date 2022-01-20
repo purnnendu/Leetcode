56e3f53f5ac0ea92a85360ee14694dcc315a7b80
purnnendu <purnnendu.1695@gmail.com>
2022-01-20 21:43:07 +0530

    119 Pascal's Triangle II using go

---
+++ 119 Pascal's Triangle II/119 Pascal's Triangle II.go
@@ -0,0 +1,13 @@
+func getRow(rowIndex int) []int {
+    res := []int{}
+
+    for i := 0; i <= rowIndex; i++ {
+        temp := make([]int, len(res))
+        copy(temp, res)
+        res = append(res, 1)
+        for j := 1; j < len(res)-1; j++ {
+            res[j] = temp[j-1] + temp[j]
+        }
+    }
+    return res
+}
