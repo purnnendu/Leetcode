c8881ba2f57962e7847a25cbb500a49d2bcb2a8e
purnnendu <purnnendu.1695@gmail.com>
2022-02-05 18:58:29 +0530

    357 Count Numbers with Unique Digits using py

---
+++ 357 Count Numbers with Unique Digits/357 Count Numbers with Unique Digits.py
@@ -0,0 +1,23 @@
+"""
+Given an integer n, return the count of all numbers with unique digits, x, where 0 <= x < 10n.
+
+Example 1:
+
+Input: n = 2
+Output: 91
+Explanation: The answer should be the total numbers in the range of 0 ≤ x < 100, excluding 11,22,33,44,55,66,77,88,99
+
+Example 2:
+
+Input: n = 0
+Output: 1
+"""
+class Solution:
+    def countNumbersWithUniqueDigits(self, n: int) -> int:
+        def count(k):
+            if k == max(10 - n, 0):
+                return 0
+            return k*(1 + count(k - 1))
+        if n == 0:
+            return 1
+        return 9*count(9) + 10
